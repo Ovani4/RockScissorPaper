fa3ba585165972280575f1d203456d082a85274d
Ovani4 <ovani4@ya.ru>
2021-01-22 01:00:33 +0300

    Create method generateFigure

--- src/main/java/com/ovani4/rockscissorpaper/GameRunner.java
+++ src/main/java/com/ovani4/rockscissorpaper/GameRunner.java
@@ -1,4 +1,9 @@
 package main.java.com.ovani4.rockscissorpaper;
 
 public class GameRunner {
+    public static void main(String[] args) {
+        Players player = new Players("Bot");
+        int x = player.generateFigure();
+        System.out.println(x);
+    }
 }
--- src/main/java/com/ovani4/rockscissorpaper/Players.java
+++ src/main/java/com/ovani4/rockscissorpaper/Players.java
@@ -4,4 +4,11 @@ public class Players {
     private String name;
     private int figure;
 
+    public Players(String name) {
+        this.name = name;
+    }
+    int generateFigure(){
+        figure = (int) (10 * Math.random());
+        return figure;
+    }
 }
