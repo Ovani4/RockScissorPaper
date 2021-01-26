0ab3e23cf742df9fa6dd114ba7ba82c7fb7e23a8
Ovani4 <ovani4@ya.ru>
2021-01-26 14:03:11 +0300

    Use Enum GameFigure, and it`s work.

--- src/main/java/com/ovani4/rockscissorpaper/GameRunner.java
+++ src/main/java/com/ovani4/rockscissorpaper/GameRunner.java
@@ -1,11 +1,5 @@
 package main.java.com.ovani4.rockscissorpaper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
 public class GameRunner {
     public static void main(String[] args) {
         GameLogic gl = new GameLogic();
